7dc1382f9680ce0ac9adef9573570b0fe11b8394
Alexandre Alves <aalves@Alexandres-MBP.lan>
2023-07-13 12:08:17 +0100

    router cleanup

--- pkg/desktop/routing/routing.ts
+++ pkg/desktop/routing/routing.ts
@@ -3,10 +3,6 @@ import NewRoot from '../pages/index.vue';
 import DesktopIndex from '../pages/desktop/index.vue';
 import General from '../pages/desktop/general.vue';
 import PortForwarding from '../pages/desktop/port-forwarding.vue';
-import Images from '../pages/images.vue';
-import ListElementalResource from '../pages/_resource/index.vue';
-import CreateElementalResource from '../pages/_resource/create.vue';
-import ElementalResourceDetails from '../pages/_resource/_id.vue';
 
 const BLANK_CLUSTER = '_';
 
@@ -47,57 +43,7 @@ const routes = [
         },
       }
     ],
-  },
-  // {
-  //   name:      `${ PRODUCT_NAME }-c-cluster-page1`,
-  //   path:      `/${ PRODUCT_NAME }/c/:cluster/page1`,
-  //   component: Dashboard,
-  //   meta:      {
-  //     product: PRODUCT_NAME,
-  //     cluster: BLANK_CLUSTER,
-  //     pkg:     PRODUCT_NAME
-  //   }
-  // },
-  // {
-  //   name:      `${ PRODUCT_NAME }-c-cluster-resource`,
-  //   path:      `/${ PRODUCT_NAME }/c/:cluster/:resource`,
-  //   component: ListElementalResource,
-  //   meta:      {
-  //     product: PRODUCT_NAME,
-  //     cluster: BLANK_CLUSTER,
-  //     pkg:     PRODUCT_NAME
-  //   }
-  // },
-  // {
-  //   name:      `${ PRODUCT_NAME }-c-cluster-resource-create`,
-  //   path:      `/${ PRODUCT_NAME }/c/:cluster/:resource/create`,
-  //   component: CreateElementalResource,
-  //   meta:      {
-  //     product: PRODUCT_NAME,
-  //     cluster: BLANK_CLUSTER,
-  //     pkg:     PRODUCT_NAME
-  //   }
-  // },
-  // {
-  //   name:      `${ PRODUCT_NAME }-c-cluster-resource-id`,
-  //   path:      `/${ PRODUCT_NAME }/c/:cluster/:resource/:id`,
-  //   component: ElementalResourceDetails,
-  //   meta:      {
-  //     product: PRODUCT_NAME,
-  //     cluster: BLANK_CLUSTER,
-  //     pkg:     PRODUCT_NAME
-  //   }
-  // },
-  // {
-  //   name:      `${ PRODUCT_NAME }-c-cluster-resource-namespace-id`,
-  //   path:      `/${ PRODUCT_NAME }/c/:cluster/:resource/:namespace/:id`,
-  //   component: ElementalResourceDetails,
-  //   meta:      {
-  //     product: PRODUCT_NAME,
-  //     cluster: BLANK_CLUSTER,
-  //     pkg:     PRODUCT_NAME
-  //   }
-  // },
+  }
 ];
 
 export default routes;
